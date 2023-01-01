13452477575cc36cff96a6ac6dfbed39b52f1b05
Hoss <geebi2003@gmail.com>
2023-01-01 21:52:33 +0200

    consistency

--- src/main/java/com/example/cms/AdminSide/ItemDetails.java
+++ src/main/java/com/example/cms/AdminSide/ItemDetails.java
@@ -41,55 +41,53 @@ public class ItemDetails extends Application {
         Main.setText(text);
         gridPane.add(text, 0, 0, 2, 2);
 
-        bigLabel l1 = new bigLabel("ID:");
+        Label l1 = new Label("ID:");
         TextField ID = new TextField(String.valueOf(product.getItemID()));
         ID.setEditable(false);
         gridPane.add(l1, 0, 3);
         gridPane.add(ID, 1, 3);
 
-        bigLabel l2 = new bigLabel("Name:");
+        Label l2 = new Label("Name:");
         TextField name = new TextField(product.getName());
         name.setEditable(false);
         gridPane.add(l2, 0, 4);
         gridPane.add(name, 1, 4);
 
-        bigLabel l3 = new bigLabel("Color:");
+        Label l3 = new Label("Color:");
         TextField color = new TextField(product.getColor());
         color.setEditable(false);
         gridPane.add(l3, 0, 5);
         gridPane.add(color, 1, 5);
 
-        bigLabel l4 = new bigLabel("Category:");
+        Label l4 = new Label("Category:");
         TextField category = new TextField(product.getCategory());
         category.setEditable(false);
         gridPane.add(l4, 0, 6);
         gridPane.add(category, 1, 6);
 
-        bigLabel l5 = new bigLabel("Size:");
+        Label l5 = new Label("Size:");
         TextField size = new TextField(String.valueOf(product.getSize()));
         size.setEditable(false);
         gridPane.add(l5, 0, 7);
         gridPane.add(size, 1, 7);
 
-        bigLabel l6 = new bigLabel("Description:");
+        Label l6 = new Label("Description:");
         TextArea description = new TextArea(String.valueOf(product.getDescription()));
         description.setEditable(false);
         gridPane.add(l6, 0, 8);
         gridPane.add(description, 1, 8);
 
-        bigLabel l7 = new bigLabel("Price:");
+        Label l7 = new Label("Price:");
         TextField price = new TextField("$" + String.valueOf(product.getBasePrice()));
         price.setEditable(false);
         gridPane.add(l7, 0, 10);
         gridPane.add(price, 1, 10);
 
-        Label l8 = new bigLabel("Quantity:");
+        Label l8 = new Label("Quantity:");
         TextField quantity = new TextField(String.valueOf(product.getQuantity()));
         quantity.setEditable(false);
         gridPane.add(l8, 0, 11);
         gridPane.add(quantity, 1, 11);
-
-        bigLabel l9 = new bigLabel("Image Source");
 
         Button back = new Button("Back");
         back.setOnAction(e -> {
@@ -100,14 +98,12 @@ public class ItemDetails extends Application {
                 throw new RuntimeException(ex);
             }
         });
+
         gridPane.add(back, 0, 12);
 
         Button edit = new Button("Edit Item");
         edit.setOnAction(e -> {
-
-
             EditItem editItem = new EditItem(inv, id);
-
             try {
                 editItem.start(stage);
             } catch (IOException | ClassNotFoundException ex) {
@@ -149,11 +145,4 @@ public class ItemDetails extends Application {
         }
     }
 
-    static class bigLabel extends Label {
-        public bigLabel(String name) {
-            super(name);
-            fontProperty().set(Font.font("arial", FontWeight.BOLD, 20));
-
-        }
-    }
 }
