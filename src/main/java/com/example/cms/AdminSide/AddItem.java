cb6b382e7737e3021740a0747fd9c8b89281b489
Hoss <geebi2003@gmail.com>
2023-01-03 01:42:15 +0200

    pker;galpalaertg[ararega

--- src/main/java/com/example/cms/AdminSide/AddItem.java
+++ src/main/java/com/example/cms/AdminSide/AddItem.java
@@ -32,36 +32,30 @@ public class AddItem extends Application {
         GridPane gridPane = Main.GetDefaultPane();
         Text title = new Text("Enter the item's details");
         title.setFont(Font.font("arial", FontWeight.BOLD, 18));
-        gridPane.add(title , 0 , 0 , 2 ,2);
+        gridPane.add(title, 0, 0, 2, 2);
 
-        Label l1 = new Label("Item ID");
+        Label l1 = new Label("Item ID: ");
         TextField ID = new TextField();
-        gridPane.add(l1,0 , 3);
-        gridPane.add(ID , 1 , 3);
+        gridPane.add(l1, 0, 3);
+        gridPane.add(ID, 1, 3);
 
 
-        Label l2 = new Label("Name");
+        Label l2 = new Label("Name: ");
         TextField name = new TextField();
-        gridPane.add(l2,0 , 4);
-        gridPane.add(name , 1 , 4);
-        // String color;
+        gridPane.add(l2, 0, 4);
+        gridPane.add(name, 1, 4);
 
-        Label l3 = new Label("Color");
-//        ColorPicker colorPicker = new ColorPicker();
-//        colorPicker.setOnAction(e->
-//        {
-//
-//        });
+        Label l3 = new Label("Color: ");
 
-        TextField  color = new TextField();
-        gridPane.add(l3,0 , 5);
-        gridPane.add(color , 1 , 5);
+        TextField color = new TextField();
+        gridPane.add(l3, 0, 5);
+        gridPane.add(color, 1, 5);
 
         Label l4 = new Label("Category: ");
         ComboBox<String> category = new ComboBox<>();
         Main.getCategories(category);
-        Label sub_category = new Label("Sub Category: ");
-        ComboBox<String>subCategory = new ComboBox<>();
+        Label sub_category = new Label("Subcategory: ");
+        ComboBox<String> subCategory = new ComboBox<>();
         Main.getSubCategories(subCategory);
         HBox hBox = new HBox();
         hBox.getChildren().addAll(l4);
@@ -69,52 +63,47 @@ public class AddItem extends Application {
 
         HBox hBox1 = new HBox();
         hBox1.setAlignment(Pos.CENTER_LEFT);
-        hBox1.getChildren().addAll(category ,sub_category, subCategory);
+        hBox1.getChildren().addAll(category, sub_category, subCategory);
         hBox1.setSpacing(20);
-        gridPane.add(hBox,0 , 6);
-        gridPane.add(hBox1, 1 , 6);
+        gridPane.add(hBox, 0, 6);
+        gridPane.add(hBox1, 1, 6);
 
 
-
-        Label l5 = new Label("Size");
+        Label l5 = new Label("Size: ");
         ComboBox size = new ComboBox();
-        size.getItems().addAll("XXS" ,"XS" , "S" , "M" , "L" , "XL" , "XXL" , "XXXL");
-        gridPane.add(l5,0 , 7);
-        gridPane.add(size , 1 , 7);
+        size.getItems().addAll("XXS", "XS", "S", "M", "L", "XL", "XXL", "XXXL");
+        gridPane.add(l5, 0, 7);
+        gridPane.add(size, 1, 7);
 
-        Label l6 = new Label("description");
+        Label l6 = new Label("Description: ");
         TextArea description = new TextArea();
-        gridPane.add(l6,0 , 8);
-        gridPane.add(description , 1 , 9);
+        gridPane.add(l6, 0, 8);
+        gridPane.add(description, 1, 9);
 
-        Label l7 = new Label("Price");
-        TextField  basePrice = new TextField();
-        gridPane.add(l7,0 , 10);
-        gridPane.add(basePrice , 1 , 10);
+        Label l7 = new Label("Price: ");
+        TextField basePrice = new TextField();
+        gridPane.add(l7, 0, 10);
+        gridPane.add(basePrice, 1, 10);
 
-        Label l8 = new Label("Quantity");
-        TextField  quantity = new TextField();
-        gridPane.add(l8,0 , 11);
-        gridPane.add(quantity , 1 , 11);
-        Label l9 = new Label("Image source");
+        Label l8 = new Label("Quantity: ");
+        TextField quantity = new TextField();
+        gridPane.add(l8, 0, 11);
+        gridPane.add(quantity, 1, 11);
+        Label l9 = new Label("Image source: ");
         TextField ImageSource = new TextField();
-        gridPane.add(l9 , 0 , 12);
-        gridPane.add(ImageSource , 1 , 12);
+        gridPane.add(l9, 0, 12);
+        gridPane.add(ImageSource, 1, 12);
 
         Button add = Main.getDefaultButton("Add");
 
-        add.setOnAction(e->{
-            if(name.getText().equals("") || color.getText().equals("") || category.getValue().equals("") || size.getSelectionModel().getSelectedItem() == null || description.getText().equals("") || basePrice.getText().equals("") || quantity.getText().equals("") || ImageSource.getText().equals("") || category.getSelectionModel().getSelectedItem() == null)
-            {
+        add.setOnAction(e -> {
+            if (name.getText().equals("") || color.getText().equals("") || category.getValue().equals("") || size.getSelectionModel().getSelectedItem() == null || description.getText().equals("") || basePrice.getText().equals("") || quantity.getText().equals("") || ImageSource.getText().equals("") || category.getSelectionModel().getSelectedItem() == null) {
                 Alert alert = new Alert(Alert.AlertType.ERROR);
                 alert.setTitle("Error");
                 alert.setHeaderText("Please fill all the fields");
                 alert.showAndWait();
                 return;
-            }
-
-            else if(!Validations.isInt(ID))
-            {
+            } else if (!Validations.isInt(ID)) {
                 Alert alert = new Alert(Alert.AlertType.ERROR);
                 alert.setTitle("Error");
                 alert.setHeaderText("Please enter a valid ID");
@@ -122,7 +111,7 @@ public class AddItem extends Application {
                 return;
             }
 
-            if(Validations.idExists( inv, Integer.parseInt(ID.getText()))) // so that we don't add same id
+            if (Validations.idExists(inv, Integer.parseInt(ID.getText()))) // so that we don't add same id
             {
                 ID.setText("ID already exists");
                 ID.setStyle("-fx-text-fill: red");
@@ -130,25 +119,19 @@ public class AddItem extends Application {
                 Alert alert = new Alert(Alert.AlertType.ERROR);
                 alert.setTitle("Error");
                 alert.setHeaderText("ID already exists");
-                alert.showAndWait();}
-
-             else if (!Validations.isDouble((basePrice)))
-            {
+                alert.showAndWait();
+            } else if (!Validations.isDouble((basePrice))) {
                 Alert alert = new Alert(Alert.AlertType.ERROR);
                 alert.setTitle("Error");
                 alert.setHeaderText("Please enter a valid price");
                 alert.showAndWait();
-            }
-            else if(!Validations.isInt(quantity))
-            {
+            } else if (!Validations.isInt(quantity)) {
                 Alert alert = new Alert(Alert.AlertType.ERROR);
                 alert.setTitle("Error");
                 alert.setHeaderText("Please enter a valid quantity");
                 alert.showAndWait();
-            }
-            else
-            {
-                Product p = new Product(Integer.parseInt(ID.getText()) , name.getText() , color.getText() , category.getValue() , (String) size.getSelectionModel().getSelectedItem(), description.getText() , Double.parseDouble(basePrice.getText()) , Integer.parseInt(quantity.getText()) , ImageSource.getText() , subCategory.getValue());
+            } else {
+                Product p = new Product(Integer.parseInt(ID.getText()), name.getText(), color.getText(), category.getValue(), (String) size.getSelectionModel().getSelectedItem(), description.getText(), Double.parseDouble(basePrice.getText()), Integer.parseInt(quantity.getText()), ImageSource.getText(), subCategory.getValue());
                 try {
                     inv.addProduct(p);
                 } catch (IOException ex) {
@@ -170,8 +153,6 @@ public class AddItem extends Application {
         EditItem.ItemButtons(stage, gridPane, add);
 
     }
-
-
 
 
 }
--- src/main/java/com/example/cms/AdminSide/EditItem.java
+++ src/main/java/com/example/cms/AdminSide/EditItem.java
@@ -37,39 +37,31 @@ public class EditItem extends Application {
         inv = new Inventory();
         Text title = new Text("Edit the item's details");
         title.setFont(Font.font("arial", FontWeight.BOLD, 18));
-        gridPane.add(title , 0 , 0 , 2 ,2);
+        gridPane.add(title, 0, 0, 2, 2);
 
-        Label l1 = new Label("Item ID");
+        Label l1 = new Label("Item ID:");
         TextField ID = new TextField();
         ID.setText(String.valueOf(product.getItemID()));
-        gridPane.add(l1,0 , 3);
-        gridPane.add(ID , 1 , 3);
+        gridPane.add(l1, 0, 3);
+        gridPane.add(ID, 1, 3);
 
-
-
-        Label l2 = new Label("Name");
+        Label l2 = new Label("Name:");
         TextField name = new TextField(product.getName());
-        gridPane.add(l2,0 , 4);
-        gridPane.add(name , 1 , 4);
-        // String color;
+        gridPane.add(l2, 0, 4);
+        gridPane.add(name, 1, 4);
 
-        Label l3 = new Label("Color");
-//        ColorPicker colorPicker = new ColorPicker();
-//        colorPicker.setOnAction(e->
-//        {
-//
-//        });
+        Label l3 = new Label("Color:");
 
-        TextField  color = new TextField(product.getColor());
-        gridPane.add(l3,0 , 5);
-        gridPane.add(color , 1 , 5);
+        TextField color = new TextField(product.getColor());
+        gridPane.add(l3, 0, 5);
+        gridPane.add(color, 1, 5);
 
         Label l4 = new Label("Category: ");
         ComboBox<String> category = new ComboBox<>();
-        category.getItems().addAll("Men" , "Women" , "Kids");
+        category.getItems().addAll("Men", "Women", "Kids");
         category.getSelectionModel().select(product.getCategory());
-        Label sub_category = new Label("Sub Category: ");
-        ComboBox<String>subCategory = new ComboBox<>();
+        Label sub_category = new Label("Subcategory: ");
+        ComboBox<String> subCategory = new ComboBox<>();
 
         subCategory.getSelectionModel().select(product.getSubCategory());
 
@@ -80,62 +72,57 @@ public class EditItem extends Application {
 
         HBox hBox1 = new HBox();
         hBox1.setAlignment(Pos.CENTER_LEFT);
-        hBox1.getChildren().addAll(category ,sub_category, subCategory);
+        hBox1.getChildren().addAll(category, sub_category, subCategory);
         hBox1.setSpacing(20);
-        gridPane.add(hBox,0 , 6);
-        gridPane.add(hBox1, 1 , 6);
+        gridPane.add(hBox, 0, 6);
+        gridPane.add(hBox1, 1, 6);
 
-        Label l5 = new Label("Size");
+        Label l5 = new Label("Size:");
         ComboBox size = new ComboBox();
         size.getSelectionModel().select(product.getSize());
-        size.getItems().addAll("XXS" ,"XS" , "S" , "M" , "L" , "XL" , "XXL" , "XXXL");
-        gridPane.add(l5,0 , 7);
-        gridPane.add(size , 1 , 7);
+        size.getItems().addAll("XXS", "XS", "S", "M", "L", "XL", "XXL", "XXXL");
+        gridPane.add(l5, 0, 7);
+        gridPane.add(size, 1, 7);
 
-        Label l6 = new Label("description");
+        Label l6 = new Label("Description:");
         TextArea description = new TextArea(product.getDescription());
-        gridPane.add(l6,0 , 8);
-        gridPane.add(description , 1 , 9);
+        gridPane.add(l6, 0, 8);
+        gridPane.add(description, 1, 9);
 
-        Label l7 = new Label("Price");
-        TextField  basePrice = new TextField( String.valueOf(product.getBasePrice()) );
-        gridPane.add(l7,0 , 10);
-        gridPane.add(basePrice , 1 , 10);
+        Label l7 = new Label("Price:");
+        TextField basePrice = new TextField(String.valueOf(product.getBasePrice()));
+        gridPane.add(l7, 0, 10);
+        gridPane.add(basePrice, 1, 10);
 
-        Label l8 = new Label("Quantity");
-        TextField  quantity = new TextField(String.valueOf(product.getQuantity()) );
-        gridPane.add(l8,0 , 11);
-        gridPane.add(quantity , 1 , 11);
+        Label l8 = new Label("Quantity:");
+        TextField quantity = new TextField(String.valueOf(product.getQuantity()));
+        gridPane.add(l8, 0, 11);
+        gridPane.add(quantity, 1, 11);
 
-        Label l9 = new Label("ImageSource");
-        TextField  ImageSource = new TextField(product.ImageSource);
-        gridPane.add(l9,0 , 12);
-        gridPane.add(ImageSource , 1 , 12);
+        Label l9 = new Label("Image Source:");
+        TextField ImageSource = new TextField(product.ImageSource);
+        gridPane.add(l9, 0, 12);
+        gridPane.add(ImageSource, 1, 12);
 
 
         Button edit = Main.getDefaultButton("Edit");
-        edit.setOnAction(e->{
+        edit.setOnAction(e -> {
 
-            if(Validations.isInt(ID) && Validations.isDouble(basePrice) && Validations.isInt(quantity))
-            {
-                if(name.getText().equals("") || color.getText().equals("") || category.getValue().equals("") || size.getSelectionModel().getSelectedItem() == null || description.getText().equals("") || basePrice.getText().equals("") || quantity.getText().equals("") || ImageSource.getText().equals("") || category.getSelectionModel().getSelectedItem() == null)
-                {
+            if (Validations.isInt(ID) && Validations.isDouble(basePrice) && Validations.isInt(quantity)) {
+                if (name.getText().equals("") || color.getText().equals("") || category.getValue().equals("") || size.getSelectionModel().getSelectedItem() == null || description.getText().equals("") || basePrice.getText().equals("") || quantity.getText().equals("") || ImageSource.getText().equals("") || category.getSelectionModel().getSelectedItem() == null) {
                     Alert alert = new Alert(Alert.AlertType.ERROR);
                     alert.setTitle("Error");
                     alert.setHeaderText("Please fill all the fields");
                     alert.showAndWait();
                     return;
-                }
-
-                else if(!Validations.isInt(ID))
-                {
+                } else if (!Validations.isInt(ID)) {
                     Alert alert = new Alert(Alert.AlertType.ERROR);
                     alert.setTitle("Error");
                     alert.setHeaderText("Please enter a valid ID");
                     alert.showAndWait();
                 }
                 inv.getProducts().remove(id);
-                if(Validations.idExists( inv, Integer.parseInt(ID.getText()))) // so that we don't add same id
+                if (Validations.idExists(inv, Integer.parseInt(ID.getText()))) // so that we don't add same id
                 {
                     ID.setText("ID already exists");
                     ID.setStyle("-fx-text-fill: red");
@@ -144,28 +131,21 @@ public class EditItem extends Application {
                     alert.setTitle("Error");
                     alert.setHeaderText("ID already exists");
                     return;
-                }
-
-                else if (!Validations.isDouble((basePrice)))
-                {
+                } else if (!Validations.isDouble((basePrice))) {
                     Alert alert = new Alert(Alert.AlertType.ERROR);
                     alert.setTitle("Error");
                     alert.setHeaderText("Please enter a valid price");
                     alert.showAndWait();
                     return;
-                }
-                else if(!Validations.isInt(quantity))
-                {
+                } else if (!Validations.isInt(quantity)) {
                     Alert alert = new Alert(Alert.AlertType.ERROR);
                     alert.setTitle("Error");
                     alert.setHeaderText("Please enter a valid quantity");
                     alert.showAndWait();
                     return;
-                }
-                else
-                {
+                } else {
 
-                    Product p = new Product(Integer.parseInt(ID.getText()) , name.getText() , color.getText() , category.getValue() , (String) size.getValue(), description.getText() , Double.parseDouble(basePrice.getText()) , Integer.parseInt(quantity.getText()) , ImageSource.getText() , category.getValue());
+                    Product p = new Product(Integer.parseInt(ID.getText()), name.getText(), color.getText(), category.getValue(), (String) size.getValue(), description.getText(), Double.parseDouble(basePrice.getText()), Integer.parseInt(quantity.getText()), ImageSource.getText(), category.getValue());
                     try {
                         inv.addProduct(p);
                     } catch (IOException ex) {
@@ -182,25 +162,12 @@ public class EditItem extends Application {
             }
         });
 
-        EditItem.ItemButtons(stage, gridPane,edit );
+        EditItem.ItemButtons(stage, gridPane, edit);
     }
 
-//    public  boolean Confirm(TextField ID, TextField name, TextField color, ComboBox category, TextField size, TextArea description, TextField basePrice, TextField quantity, TextField imageSource) {
-//
-//        try {
-//            inv.getProducts().remove(id);
-//            inv.addProduct(new Product(Integer.parseInt(ID.getText()), name.getText() , color.getText() , (String) category.getSelectionModel().getSelectedItem(), size.getText() , description.getText() , Double.parseDouble(basePrice.getText()) , Integer.parseInt(quantity.getText()), imageSource.getText()));
-//        }
-//        catch (IOException ex) {
-//            throw new RuntimeException(ex);
-//        }
-//        return false;
-//    }
-
-
     static void ItemButtons(Stage stage, GridPane gridPane, Button edit) {
-        Button back = Main.getDefaultButton("Back");
-        back.setOnAction(e->{
+        Button back = Main.getDefaultButton("← Back");
+        back.setOnAction(e -> {
             AdminPanel adminPanel = new AdminPanel();
             try {
                 adminPanel.start(stage);
@@ -209,9 +176,9 @@ public class EditItem extends Application {
             }
         });
 
-        gridPane.add(edit , 2 , 14);
-        gridPane.add(back , 1 , 14);
-        stage.setScene(new Scene(gridPane , 750 , 700));
+        gridPane.add(edit, 2, 14);
+        gridPane.add(back, 1, 14);
+        stage.setScene(new Scene(gridPane, 750, 700));
         stage.show();
     }
 }
